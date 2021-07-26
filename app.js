084907ddfc66cf8ee95cc23df5d96ffa297a2c2b
liwemg <wemgli@qq.com>
2021-07-26 23:03:01 +0800

    7-26 lab

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 // app.js
 App({
-  onLaunch() {
+  //生命周期函数，是在特定时刻自动执行的函数
+  //小程序启动时，自动执行
+  onLaunch(options) {
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
+    //console.log('hello', options)
 
     // 登录
     wx.login({
@@ -13,7 +16,23 @@ App({
       }
     })
   },
+  //当小程序重新展示的时候，自动执行
+  onShow(options){
+    //console.log('onShow', options)
+  },
+
+  //当小程序取消展示的时候，自动执行
+  onHide(){
+    console.log('hide')
+  },
+  //当脚本执行错误时，自动执行的函数
+  onError(msg){
+    console.log('error')
+  },
+  sayHello(){
+    return 'hello'
+  },
   globalData: {
     userInfo: null
-  }
+  },
 })
